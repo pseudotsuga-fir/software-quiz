9413cba6d4aa18fc81643a2c2229de2dd4a6ba58
Andrew H <55859785+pseudotsuga-fir@users.noreply.github.com>
2024-04-17 23:02:33 -0700

    restructure backend (#8)

--- backend/config/puma.rb
+++ backend/config/puma.rb
@@ -22,8 +22,9 @@ end
 # terminating a worker in development environments.
 worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT") { 3000 }
+# Specifies the IP and port to listen on.
+bind 'tcp://127.0.0.1:3008'
+
 
 # Specifies the `environment` that Puma will run in.
 environment ENV.fetch("RAILS_ENV") { "development" }
