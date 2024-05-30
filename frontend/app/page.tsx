039da05114acc4043f3ccb9501200508d0f79d8e
Andrew Hepworth <hepworta@oregonstate.edu>
2024-05-30 15:17:15 -0700

    fix home page redirect

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,19 +1,5 @@
-"use client";
-
-import { useUserStore } from "@/hooks/appstate";
-import styles from "./home.module.scss";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  async function fetchQuizzes() {
-    await fetch("/api/quizzes").then((response) => response.json());
-    console.log(useUserStore.getState().user);
-  }
-
-  return (
-    <div className={styles.home}>
-      <h1>{"We're up and running!"}</h1>
-      <img className={styles.pigeonPic} src="/cool_ass_pigeon.jpg" />
-      <button onClick={fetchQuizzes}>Test Auth</button>
-    </div>
-  );
+  redirect("/login");
 }
